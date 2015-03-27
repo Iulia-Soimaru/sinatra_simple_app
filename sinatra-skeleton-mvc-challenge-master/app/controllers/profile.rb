c8b1c2b3a5866a52249915dad74f7ee4332b4e92
Iulia Soimaru <yuliga4@gmail.com>
2015-03-27 16:02:49 -0700

    complete sign in and sign out forms

--- sinatra-skeleton-mvc-challenge-master/app/controllers/profile.rb
+++ sinatra-skeleton-mvc-challenge-master/app/controllers/profile.rb
@@ -1,10 +1,11 @@
 get '/profile/:user_id' do
-  @user = User.find(session[:user_id])
+  @user = User.find(params[:user_id])
   erb :profile
 end
 
-post '/profile/:user_id' do
+delete '/profile/:user_id' do
   # @user = User.find(session[:user_id])
-  session[:user_id] = nil
+  @user = User.find(params[:user_id])
+  params[:user_id] = nil
   redirect "/"
 end
--- sinatra-skeleton-mvc-challenge-master/app/controllers/sign_in.rb
+++ sinatra-skeleton-mvc-challenge-master/app/controllers/sign_in.rb
@@ -5,9 +5,9 @@ end
 post '/sign_in' do
   @signin_user = User.where(email: params[:email]).first
   if @signin_user && @signin_user.password == params[:password]
-    status 200
     session[:user_id] == @signin_user.id
-    redirect 'profile/#{@user.id}'
+    status 200
+    redirect "/profile/#{@signin_user.id}"
   else
     status 400
     "Wrong email or password"

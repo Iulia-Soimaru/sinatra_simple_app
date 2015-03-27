f6cf06c3911057c7d88d167767f50005e60e3974
Iulia Soimaru <yuliga4@gmail.com>
2015-03-27 14:31:36 -0700

    create profile page

--- sinatra-skeleton-mvc-challenge-master/app/controllers/profile.rb
+++ sinatra-skeleton-mvc-challenge-master/app/controllers/profile.rb
@@ -1,3 +1,10 @@
 get '/profile/:user_id' do
+  @user = User.find(session[:user_id])
   erb :profile
+end
+
+post '/profile/:user_id' do
+  @user = User.find(session[:user_id])
+  @user
+  redirect '/'
 end
